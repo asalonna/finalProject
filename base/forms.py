da52dca21bce037c669e0b8b551ef1a82cdac9fa
axs1485 <axs1485.student.bham.ac.uk>
2022-09-03 19:44:30 +0100

    Implement user access form.

--- base/forms.py
+++ base/forms.py
@@ -5,3 +5,7 @@ class CreateQuestionForm(forms.Form):
     question_body = forms.CharField(label='Enter your question:', max_length=500)
     question_answer = forms.CharField(label='Enter the expected answer to your question:', max_length=500)
     question_passcode = forms.CharField(label='Enter a passcode for your question:', max_length=10)
+
+class UserAccessForm(forms.Form):
+    user_id = forms.CharField(label='User ID:', max_length=10)
+    question_access_code = forms.CharField(label='Question Access Code:', max_length=10)
