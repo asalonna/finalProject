4adf834f266efe37dfc453a025c36a753f150f09
axs1485 <axs1485.student.bham.ac.uk>
2022-09-04 22:47:07 +0100

    Updated DSL

--- questionMod.py
+++ questionMod.py
@@ -39,17 +39,31 @@ class Question(object):
             
         
         for a in model.answerContents:
-            if a.repeat < 1:
-                a.repeat = 1
-            for i in range(a.repeat):
+            if a.repeat is None:
+                repeat = 1
+            elif type(a.repeat) == str:
+                repeat = self.variables[a.repeat]
+            else:
+                repeat = a.repeat
+            for i in range(repeat):
                 if a.contentType.__class__.__name__ == "Text":
                     self.answerText = self.answerText + a.contentType.x
 
                 elif a.contentType.__class__.__name__ == "Order":
+                    print(a.contentType.list)
+                    for i in range(len(a.contentType.list)):
+                        if type(a.contentType.list[i]) == str:
+                            a.contentType.list[i] = self.variables[a.contentType.list[i]]
                     a.contentType.list.sort()
                     self.answerText = self.answerText + str(a.contentType.list).strip("[]")
 
                 elif a.contentType.__class__.__name__ == "Sequence":
+                    if type(a.contentType.start) == str:
+                        a.contentType.start = self.variables[a.contentType.start]
+                    if type(a.contentType.end) == str:
+                        a.contentType.end = self.variables[a.contentType.end]
+                    if type(a.contentType.step) == str:
+                        a.contentType.step = self.variables[a.contentType.step]
                     rangeOutput = range(a.contentType.start, a.contentType.end, a.contentType.step)
                     for i in rangeOutput:
                         self.answerText = self.answerText + str(i)
@@ -87,4 +101,3 @@ def getQuestionObjectString(strDSL, question_seed):
     question = Question()
     question.interpret(question_model, question_seed)
     return question
-
