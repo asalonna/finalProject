041b356f72c8333a741c2e4ca3042df5b5f2f2b8
axs1485 <axs1485.student.bham.ac.uk>
2022-09-08 18:49:52 +0100

    Added classroom creation

--- base/urls.py
+++ base/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('task/<int:pk>', views.task, name='task'),
-    path('create', views.create_question, name='create_question'),
+    path('create_classroom', views.create_classroom, name='create_classroom'),
+    path('create_question', views.create_question, name='create_question'),
     path('access', views.access, name='access_question'),
-    path('track', views.track, name='tracking'),
+    path('task/<int:pk>', views.task, name='task'),
     path('next/<str:pk>', views.next_question, name='next'),
     path('end_screen', views.end_of_questions, name='end_screen'),
+    path('track', views.track, name='tracking'),
 ]
 
