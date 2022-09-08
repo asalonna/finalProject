7772e8452e28a572922827889730c350e5bf82ae
axs1485 <axs1485.student.bham.ac.uk>
2022-09-08 17:46:24 +0100

    Updated models.py to add classrooms

--- base/models.py
+++ base/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class ClassRoom(models.Model):
+    class_id = models.CharField(max_length=6, primary_key=True)
+    name = models.CharField(max_length=100)
+    passcode = models.CharField(max_length=10)
+
 class Questions(models.Model):
     title = models.CharField(max_length=100)
     question_and_answer = models.CharField(max_length=1000)
-    passcode = models.CharField(max_length=10)
-    class_group = models.CharField(max_length=50)
+    class_id = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
     difficulty = models.PositiveIntegerField()
-
     def __str__(self):
         return self.title
+
+class ClassUsers(models.Model):
+    class_id = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    user_id = models.CharField(max_length=10)
+    highest_difficulty = models.PositiveIntegerField()
+    last_question = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    class_completed = models.BooleanField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['class_id', 'user_id'])
+        ]
 
 class UserMarks(models.Model):
     user_id = models.CharField(max_length=10)
